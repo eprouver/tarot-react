44eef93041d653619cd8355e8eab9ae918b46b3a
Daniel Cohen <eprouver@yahoo.com>
2018-01-21 14:27:58 -0600

    reach

--- src/components/Story.js
+++ src/components/Story.js
@@ -25,6 +25,19 @@ const patterns = {
   Magnitude: 'One ( item || concept ) and how it exists in a set of contexts'
 };
 
+const affectLevels = [
+  {
+    name: 'Autonomy',
+    descrip: 'Just the self or immediate friends and family'
+  }, {
+    name: 'Community',
+    descrip: 'The wider community, town, country'
+  }, {
+    name: 'Divinity',
+    descrip: 'Humanity, morality, fundamental logic, basic truths'
+  }
+];
+
 const humors = [
   {
     name: 'Positive Repetition',
@@ -107,17 +120,19 @@ class Story extends React.Component {
       };
     });
 
-    let state = {moral: new Array(2).fill().map(makeCard),
+    let state = {
+      moral: new Array(2).fill().map(makeCard),
       plot: new Array(3).fill().map(makeCard),
-      humor: humors[~~(Math.random() * humors.length)],
-      arc: arcs[~~(Math.random() * arcs.length)],
+      humor: humors[~~ (Math.random() * humors.length)],
+      arc: arcs[~~ (Math.random() * arcs.length)],
+      affectLevel: ~~ (Math.random() * 3)
     };
 
     state.plot.forEach((c, i) => {
       c.reversed = state.arc.orientations[i];
     })
 
-    if(setState){
+    if (setState) {
       this.setState(state);
     }
 
@@ -148,7 +163,7 @@ class Story extends React.Component {
 
   render() {
     const linker = (pos, i) => {
-      if(!pos){
+      if (!pos) {
         return '';
       }
 
@@ -194,18 +209,58 @@ class Story extends React.Component {
       <div className="container-fluid text-center">
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-4">
+            <h5 className="text-muted">Reach of the Story</h5>
+            <div className="row">
+              <div className="col-4">
+                <div className="c-level">
+                  <div className={"c-circle " + (
+                      this.state.affectLevel === 2
+                      ? 'active'
+                      : '')}>
+                    <div className={"c-circle " + (
+                        this.state.affectLevel === 1
+                        ? 'active'
+                        : '')}>
+                      <div className={"c-circle " + (
+                          this.state.affectLevel === 0
+                          ? 'active'
+                          : '')}></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col">
+                <h4>{affectLevels[this.state.affectLevel].name}</h4>
+                <h5>{affectLevels[this.state.affectLevel].descrip}</h5>
+              </div>
+            </div>
+
+            <hr/>
+
+            <div id="arc" className="">
+              <h5 className="text-muted">Plot Arc:</h5>
+              <div className="row">
+                <div className="col-4">
+                <img className="mw-100" style={{
+                    transition: 'all 0.5s ease'
+                  }} src={arcImages[this.state.arc.name + '.png']}/>
+                </div>
+                <div className="col">
+                <h4>{this.state.arc.name}</h4>
+                </div>
+                </div>
+
+
+            </div>
+
+            <hr/>
+
             <div id="humor" className="">
               <h5 className="text-muted">Surprise Humor Pattern:</h5>
               <h4>{this.state.humor.name}</h4>
               <h5>{this.state.humor.descrip}</h5>
               <p className="text-muted">{this.state.humor.type}: {patterns[this.state.humor.type]}</p>
-
-            </div>
-            <hr/>
-            <div id="arc" className="">
-              <h5 className="text-muted">Plot Arc:</h5>
-              <h4>{this.state.arc.name}</h4>
-              <img className="mw-100" style={{transition: 'all 0.5s ease'}} src={arcImages[this.state.arc.name + '.png']}/>
             </div>
             <hr/>
           </div>
